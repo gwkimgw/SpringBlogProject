cd03e5513ced178c83ef5614b3517aace42f680c
gunwooKim <marianaaquaria@gmail.com>
2022-08-04 14:36:40 +0900

    Feat http test class add get, post, put, delete method in controller class

--- src/main/java/controller/HttpControllerTest.java
+++ src/main/java/controller/HttpControllerTest.java
@@ -1,4 +1,26 @@
 package controller;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
 public class HttpControllerTest {
+    @GetMapping("/http/get")
+    public String getTest(){
+        return "get";
+    }
+
+    @PostMapping("/http/post")
+    public String postTest(){
+        return "post";
+    }
+
+    @PutMapping("http/put")
+    public String putTest(){
+        return "put";
+    }
+
+    @DeleteMapping("/http/delete")
+    public String deleteTest(){
+        return "delete";
+    }
 }
