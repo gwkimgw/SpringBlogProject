68125777f6da4ca503be13a1903e6564c9b68072
gunwooKim <marianaaquaria@gmail.com>
2022-08-09 14:57:31 +0900

    feat: Add UserRepository to import save() Autowired UserRepository iterface extending JpaRepository which contains save method

---
+++ src/main/java/com/cos/springblogproject/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.cos.springblogproject.repository;
+
+import com.cos.springblogproject.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+//@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+}
--- src/main/java/com/cos/springblogproject/test/DummyControllerTest.java
+++ src/main/java/com/cos/springblogproject/test/DummyControllerTest.java
@@ -1,16 +1,24 @@
 package com.cos.springblogproject.test;
 
 import com.cos.springblogproject.model.User;
+import com.cos.springblogproject.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DummyControllerTest {
+    //Dependency Injection
+    @Autowired
+    private UserRepository userRepository;
+
     @PostMapping("/dummy/join")
     public String join(User user){
         System.out.println(user.getUsername()
                 + " " + user.getPassword()
                 + " " + user.getEmail());
+
+        userRepository.save(user);
         return "new member joined";
     }
 }
