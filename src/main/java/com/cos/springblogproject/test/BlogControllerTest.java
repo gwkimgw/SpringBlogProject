c5c508b2d6e255d46b156e6a7fe8f8c3fee8b1b3
gunwooKim <marianaaquaria@gmail.com>
2022-08-09 14:49:13 +0900

    refactor: change directory moved classes under blog to test package

--- src/main/java/com/cos/springblogproject/test/BlogControllerTest.java
+++ src/main/java/com/cos/springblogproject/test/BlogControllerTest.java
@@ -1,4 +1,4 @@
-package com.cos.springblogproject.controller;
+package com.cos.springblogproject.test;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
