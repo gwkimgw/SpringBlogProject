5ef4fb1ee1452be8d70d4e8eccbb0b30cb98882d
gunwooKim <marianaaquaria@gmail.com>
2022-08-10 15:14:02 +0900

    feat: Add user select query Wrote detail function to select a user by userId

--- src/main/java/com/cos/springblogproject/test/DummyControllerTest.java
+++ src/main/java/com/cos/springblogproject/test/DummyControllerTest.java
@@ -4,14 +4,30 @@ import com.cos.springblogproject.model.RoleType;
 import com.cos.springblogproject.model.User;
 import com.cos.springblogproject.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.function.Supplier;
 
 @RestController
 public class DummyControllerTest {
     //Dependency Injection
     @Autowired
     private UserRepository userRepository;
+
+    @GetMapping("/dummy/user/{id}")
+    public User detail(@PathVariable int id){
+        User user = userRepository.findById(id).orElseThrow(
+                new Supplier<IllegalArgumentException>() {
+            @Override
+            public IllegalArgumentException get() {
+                return new IllegalArgumentException("no such user");
+            }
+        });
+        return user;
+    }
 
     @PostMapping("/dummy/join")
     public String join(User user){
