29e0c3fccfbfdcc37c88187f56a8f2b6c2ea87a4
gunwooKim <marianaaquaria@gmail.com>
2022-08-12 12:46:47 +0900

    feat: Add BoardController to connect index page
    Wired index() to index.jsp under WEB-INF package

---
+++ src/main/java/com/cos/springblogproject/controller/BoardController.java
@@ -0,0 +1,13 @@
+package com.cos.springblogproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BoardController {
+    @GetMapping("/")
+    public String index(){
+        //return index.jsp
+        return "index";
+    }
+}
