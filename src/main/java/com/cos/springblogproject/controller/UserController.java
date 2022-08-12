c30f06d516d60c31bd37c14596099d5fc72453b0
gunwooKim <marianaaquaria@gmail.com>
2022-08-12 13:34:43 +0900

    feat: Add loginForm.jsp
    Add loginForm mapping at UserController.java

--- src/main/java/com/cos/springblogproject/controller/UserController.java
+++ src/main/java/com/cos/springblogproject/controller/UserController.java
@@ -9,4 +9,9 @@ public class UserController {
     public String joinForm(){
         return "user/joinForm";
     }
+
+    @GetMapping("/user/loginForm")
+    public String loginForm(){
+        return "user/loginForm";
+    }
 }
