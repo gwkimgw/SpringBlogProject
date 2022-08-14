a68bfe52a648939b24ee8ecce9ca1ccbbb86b0d6
gunwooKim <marianaaquaria@gmail.com>
2022-08-14 13:11:02 +0900

    feat: Add user role

--- src/main/java/com/cos/springblogproject/controller/api/UserApiController.java
+++ src/main/java/com/cos/springblogproject/controller/api/UserApiController.java
@@ -1,7 +1,10 @@
 package com.cos.springblogproject.controller.api;
 
 import com.cos.springblogproject.dto.ResponseDto;
+import com.cos.springblogproject.model.RoleType;
 import com.cos.springblogproject.model.User;
+import com.cos.springblogproject.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,9 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserApiController {
+
+    @Autowired
+    private UserService userService;
+
     @PostMapping("/api/user")
     public ResponseDto<Integer> save(@RequestBody User user) {
         System.out.println("userapicontroller");
-        return new ResponseDto<Integer>(HttpStatus.OK, 1);
+        user.setRole(RoleType.USER);
+        int result = userService.join(user);
+        return new ResponseDto<Integer>(HttpStatus.OK, result);
     }
 }
