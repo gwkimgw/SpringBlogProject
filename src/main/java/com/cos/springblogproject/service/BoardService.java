02dc2fb36dab00caecf2f6c6f7c0e36f81c84da4
gunwooKim <marianaaquaria@gmail.com>
2022-08-21 14:34:44 +0900

    feat: Add article delete
    Get id as text from jsp and delete the board

--- src/main/java/com/cos/springblogproject/service/BoardService.java
+++ src/main/java/com/cos/springblogproject/service/BoardService.java
@@ -1,14 +1,11 @@
 package com.cos.springblogproject.service;
 
 import com.cos.springblogproject.model.Board;
-import com.cos.springblogproject.model.RoleType;
 import com.cos.springblogproject.model.User;
 import com.cos.springblogproject.repository.BoardRepository;
-import com.cos.springblogproject.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,7 +16,7 @@ public class BoardService {
     @Autowired
     private BoardRepository boardRepository;
 
-    @Transactional
+    @Transactional(readOnly = true)
     public void write(Board board, User user) {
         board.setCount(0);
         board.setUser(user);
@@ -30,10 +27,16 @@ public class BoardService {
         return boardRepository.findAll(pageable);
     }
 
+    @Transactional(readOnly = true)
     public Board getContent(int id) {
         return boardRepository.findById(id)
                 .orElseThrow(() -> {
                     return new IllegalArgumentException("failed: no such id");
                 });
+    }
+
+    @Transactional
+    public void delete(int id) {
+        boardRepository.deleteById(id);
     }
 }
--- src/main/webapp/WEB-INF/views/board/detail.jsp
+++ src/main/webapp/WEB-INF/views/board/detail.jsp
@@ -5,6 +5,11 @@
      <button id = "btn-edit" class="btn btn-warning">edit</button>
      <button id = "btn-delete" class="btn btn-danger">delete</button>
      <br/><br/>
+     <div>
+         article number: <span id="id"><i>${board.id} </i></span>
+         writer: <span><i>${board.user.username} </i></span>
+     </div>
+     <br/>
      <div class="form-group">
          <h3>${board.title}</h3>
      </div>
