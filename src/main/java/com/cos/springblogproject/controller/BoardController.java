2994686fb16b51349133c8463b543ba49dde663c
gunwooKim <marianaaquaria@gmail.com>
2022-08-22 11:39:45 +0900

    feat: Add article update
    Change edit button to edit link and map it with id at BoardController

--- src/main/java/com/cos/springblogproject/controller/BoardController.java
+++ src/main/java/com/cos/springblogproject/controller/BoardController.java
@@ -31,4 +31,10 @@ public class BoardController {
     public String saveForm() {
         return "/board/saveForm";
     }
+
+    @GetMapping("/board/{id}/updateForm")
+    public String updateForm (@PathVariable int id, Model model) {
+        model.addAttribute("board", boardService.getContent(id));
+        return "board/updateForm";
+    }
 }
