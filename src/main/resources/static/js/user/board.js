a12bc185e6f7cbbdf652033f4875671c425ac2c4
gunwooKim <marianaaquaria@gmail.com>
2022-08-22 12:19:45 +0900

    feat: Add ';' after datas

--- src/main/resources/static/js/user/board.js
+++ src/main/resources/static/js/user/board.js
@@ -7,13 +7,17 @@ let index  = {
         $("#btn-delete").on("click", ()=>{
             this.deleteById();
         });
+
+        $("#btn-edit").on("click", ()=>{
+            this.update();
+        });
     },
 
     save: function() {
         let data = {
             title: $("#title").val(),
             content: $("#content").val()
-        }
+        };
 
         $.ajax({
             type: "POST",
@@ -30,7 +34,7 @@ let index  = {
     },
 
     deleteById: function() {
-        var id = $("#id").text();
+        let id = $("#id").text();
 
         $.ajax({
             type: "DELETE",
@@ -38,6 +42,28 @@ let index  = {
             dataType: "json"
         }).done(function (resp) {
             alert("article deleted");
+            location.href = "/";
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
+    },
+
+    update: function() {
+        let id = $("#id").val();
+
+        let data = {
+            title: $("#title").val(),
+            content: $("#content").val()
+        };
+
+        $.ajax({
+            type: "PUT",
+            url: "/api/board/"+id,
+            data: JSON.stringify(data),
+            contentType: "application/json; charset=utf-8",
+            dataType: "json"
+        }).done(function (resp) {
+            alert("article updated");
             location.href = "/";
         }).fail(function (error) {
             alert(JSON.stringify(error));
