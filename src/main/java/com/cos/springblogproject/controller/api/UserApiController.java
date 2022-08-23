b7ea644c760c7edc2ea0b5c4e97cfc4155c26a96
gunwooKim <marianaaquaria@gmail.com>
2022-08-23 12:47:05 +0900

    feat: Tried to force inject authentication to session, failed Tried to recreated automatic session update by pushing UsernamePasswordAuthenticationToken to Authentication

--- src/main/java/com/cos/springblogproject/controller/api/UserApiController.java
+++ src/main/java/com/cos/springblogproject/controller/api/UserApiController.java
@@ -1,11 +1,17 @@
 package com.cos.springblogproject.controller.api;
 
+import com.cos.springblogproject.config.auth.PrincipalDetail;
 import com.cos.springblogproject.dto.ResponseDto;
 import com.cos.springblogproject.model.RoleType;
 import com.cos.springblogproject.model.User;
 import com.cos.springblogproject.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -29,8 +35,16 @@ public class UserApiController {
     }
 
     @PutMapping("/user")
-    public ResponseDto<Integer> update(@RequestBody User user) {
+    public ResponseDto<Integer> update(@RequestBody User user
+//            , @AuthenticationPrincipal PrincipalDetail principal, HttpSession session
+            ) {
         userService.update(user);
+//        Authentication authentication =
+//                new UsernamePasswordAuthenticationToken(principal, null);
+//        SecurityContext securityContext = SecurityContextHolder.getContext();
+//        securityContext.setAuthentication(authentication);
+//        session.setAttribute("SPRING_SECURITY_CONTEXT", securityContext);
+
         return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
     }
 
