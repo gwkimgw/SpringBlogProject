d57f25546a4256dc1dc4c0ec6257b8cf607fb0d6
gunwooKim <marianaaquaria@gmail.com>
2022-08-25 13:40:57 +0900

    feat: Add kakao user automatic join and login
    If user is not signed in(checked by username at UserService), generate username and set default password to user and login

--- src/main/java/com/cos/springblogproject/model/User.java
+++ src/main/java/com/cos/springblogproject/model/User.java
@@ -22,7 +22,7 @@ public class User {
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private int id;
 
-    @Column(nullable = false, length = 30)
+    @Column(nullable = false, length = 100)
     private String username;
 
     @Column(nullable = false, length = 100)
